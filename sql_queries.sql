3a4ea87a5616492ca6a135bcd655c121ba17e104
ysabellakristine <167633653+ysabellakristine@users.noreply.github.com>
2024-09-03 23:25:33 +0800

    Update sql_queries.sql

--- sql_queries.sql
+++ sql_queries.sql
@@ -19,3 +19,31 @@ SELECT GroupName
 FROM Groups;
 
 -- Number 4 --
+SELECT 
+    GroupMemberUserID AS User_ID, 
+    GroupMemberShipRequestsID, 
+    GroupID, 
+    IsGroupMemberShipAccepted
+FROM GroupMembershipRequests
+WHERE GroupMemberUserID = 2;
+
+-- Number 5 --
+SELECT 
+us.UserID, 
+us.Username, 
+us.FirstName, 
+us.LastName
+FROM Users us
+JOIN Friends fr ON (us.UserID = fr.FriendWhoAdded OR us.UserID = fr.FriendBeingAdded)
+WHERE (fr.FriendWhoAdded = 2 OR fr.FriendBeingAdded = 2)
+AND fr.IsAccepted = TRUE AND us.UserID != 2;
+-- Number 6 --
+
+
+-- Number 7 -- 
+SELECT * FROM Posts
+WHERE GroupID = 2;
+
+-- Number 8 -- 
+SELECT * FROM GroupMembershipRequests 
+WHERE GroupID = 2 AND IsGroupMemberShipAccepted = FALSE;
