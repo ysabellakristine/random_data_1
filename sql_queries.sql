4e9b274bee95b958660ccf624e5ff23bd4fa50b3
ysabellakristine <167633653+ysabellakristine@users.noreply.github.com>
2024-09-03 22:49:49 +0800

    Update sql_queries.sql

--- sql_queries.sql
+++ sql_queries.sql
@@ -3,4 +3,19 @@ SELECT * FROM Users
 WHERE DateOfBirth BETWEEN '2000-01-01' AND '2004-12-31';
 
 -- Number 2 -- 
+SELECT 
+    PostedBy AS User_ID, 
+    PostID, 
+    PostDescription, 
+    IsPublic, 
+    IsOnlyForFriends, 
+    GroupID, 
+    DatePosted
+FROM Posts
+WHERE PostedBy = 4;
 
+-- Number 3 -- 
+SELECT GroupName
+FROM Groups;
+
+-- Number 4 --
